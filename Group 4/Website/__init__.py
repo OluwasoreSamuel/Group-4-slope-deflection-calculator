1add933ae5a7c3dce4aba7c2103c39ea25ff9222
Oluwasoresamuel <oluwasoresamuel@gmail.com>
2024-02-03 21:32:52 +0100

    linked page

--- Group 4/Website/__init__.py
+++ Group 4/Website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 
 def create_app():
   app = Flask(__name__)
--- Group 4/main.py
+++ Group 4/main.py
@@ -1,10 +1,14 @@
-from Website import create_app,render_template
+from Website import create_app,render_template,Blueprint
 
 app = create_app()
 
 @app.route("/")
 def homepage():
-    return render_template("home.html")
+    return render_template("index.html")
+
+@app.route("/mainpage")
+def mainpage():
+    return render_template("main.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
